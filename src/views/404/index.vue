a320662099531a5c39b6dd37591d0e73fdfa9f06
@xiaodutongxue111 <1577924928@qq.com>
2024-04-16 18:35:23 +0800

    "404页面完善"

--- src/views/404/index.vue
+++ src/views/404/index.vue
@@ -1,11 +1,47 @@
 <template>
-    <div>
-        404 页面
+    <div class="error_box">
+        <img src="@/assets/404.jpg" alt="">
+        <div class="return_box" @click="back">
+            返回首页
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
+import router from '@/router';
 
+
+function back() {
+    router.push('/')
+}
 </script>
 
-<style scoped></style>
+<style scoped lang="scss">
+.error_box {
+    width: 100%;
+    height: 100vh;
+    position: relative;
+
+    img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
+
+    .return_box {
+        position: absolute;
+        right: 10%;
+        top: 20%;
+        transform: translate(-50%, -50%);
+        font-size: 30px;
+        background-color: #f56c6c;
+        padding: 10px 30px;
+        border-radius: 10px;
+        color: #fff;
+
+        &:hover {
+            cursor: pointer;
+        }
+    }
+}
+</style>
